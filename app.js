810a97c7704b17fc5ac7b70db51503d9764042c3
aladdin4u <taofikakinkumi@gmail.com>
2023-02-18 09:28:03 +0100

    initial commit

--- app.js
+++ app.js
@@ -1,63 +1,45 @@
 const express = require("express");
-const mysql = require("mysql2");
+const { createClient } = require("@supabase/supabase-js");
 require("dotenv").config();
 
 const app = express();
 
-const connection = mysql.createConnection({
-  host: "localhost",
-  user: process.env.USER,
-  password: process.env.PSD,
-  database: process.env.DB,
-});
-
-connection.connect();
+const supabaseUrl = process.env.SUPABASE_URL;
+const supabaseKey = process.env.SUPABASE_KEY;
+const supabase = createClient(supabaseUrl, supabaseKey);
 
 app.set("view engine", "ejs");
 app.use(express.static("public"));
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
-app.get("/", (req, res) => {
-  let sql =
-    "select * from customer left join planner on planner.order_date between curdate() and date_add(curdate(), interval 7 day) order by order_date";
-  connection.query(sql, function (error, results) {
-    if (error) throw error;
-    // console.log(results)
-    res.render("index.ejs", { cus: results });
-  });
+app.get("/", async (req, res) => {
+  let { data: customer, error } = await supabase.from("customer").select("*");
+  let { data: planner, err } = await supabase
+    .from("planner")
+    .select("*")
+    .order("date");
+  // console.log(planner);
+  res.render("index.ejs", { cus: customer, plan: planner });
 });
 
-app.get("/planner", (req, res) => {
-  let sql =
-    "select DISTINCT * from planner where order_date between curdate() and date_add(curdate(), interval 6 day) order by order_date";
+app.post("/addslot", async (req, res) => {
+  // console.log(req.body);
+  let slot1 = req.body.slotNumber1;
+  let slot2 = req.body.slotNumber2;
+  let slot3 = req.body.slotNumber3;
+  let slot4 = req.body.slotNumber4;
+  let date = req.body.date;
 
-  connection.query(sql, function (error, results) {
-    if (error) throw error;
-    // console.log(results)
-    res.json(results);
-  });
-});
-
-app.get("/planners", (req, res) => {
-  let sql =
-    "select order_date, concat(c1.customer_name, ' (', c1.pickup_location, ' to ', c1.dropoff_location, ')') as slot_1_customer,concat(c2.customer_name, ' (', c2.pickup_location, ' to ', c2.dropoff_location, ')') as slot_2_customer,concat(c3.customer_name, ' (', c3.pickup_location, ' to ', c3.dropoff_location, ')') as slot_3_customer,concat(c4.customer_name, ' (', c4.pickup_location, ' to ', c4.dropoff_location, ')') as slot_4_customer from planner left join customer c1 on planner.slot_1_customer_id = c1.customer_id left join customer c2 on planner.slot_2_customer_id = c2.customer_id left join customer c3 on planner.slot_3_customer_id = c3.customer_id left join customer c4 on planner.slot_4_customer_id = c4.customer_id where order_date between curdate() and date_add(curdate(), interval 6 day) order by order_date";
-
-  connection.query(sql, function (error, results) {
-    if (error) throw error;
-    res.render("planner.ejs", { plan: results });
-    // res.json(results);
-  });
-});
-
-app.post("/addslot", (req, res) => {
-  console.log(req.body);
-  let sql =
-    "update planner set slot_1_customer_id = req.body.slotNumber1 , slot_2_customer_id = req.body.slotNumber2, slot_3_customer_id = req.body.slotNumber3, slot_4_customer_id = req.body.slotNumber4 where order_date = req.body.date'";
-  connection.query(sql, function (error, results) {
-    if (error) throw error;
-    console.log(results);
-  });
+  let { data, error } = await supabase
+    .from("planner")
+    .update({
+      slot_1: slot1,
+      slot_2: slot2,
+      slot_3: slot3,
+      slot_4: slot4,
+    })
+    .eq("date", date);
   res.redirect("/");
 });
 
